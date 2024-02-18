8419d94a40c7b6a2fb1d82d0559e05f8b2f7d2ed
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-02-18 11:16:14 +0000

    chore: godot

--- alzmanagementgroup_test.go
+++ alzmanagementgroup_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// TestGoGetter
+// TestGoGetter.
 func getRemoteLib(ctx context.Context) (fs.FS, error) {
 	u := url.URL{
 		Scheme: "https",
