a74a984c146cdc77df86cb57f1a5c674d7a243d9
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-07-07 09:39:53 +0100

    feat: add yaml tags to input fields (#134)

--- processor/libDefaultpolicyValues.go
+++ processor/libDefaultpolicyValues.go
@@ -6,10 +6,10 @@ package processor
 // LibDefaultPolicyValues represents the default policy values that allow a single value to be mapped into different assignments.
 type LibDefaultPolicyValues struct {
 	Defaults []struct {
-		DefaultName       string `json:"default_name"`
+		DefaultName       string `json:"default_name" yaml:"default_name"`
 		PolicyAssignments []struct {
-			PolicyAssignmentName string   `json:"policy_assignment_name"`
-			ParameterNames       []string `json:"parameter_names"`
-		} `json:"policy_assignments"`
-	} `json:"defaults"`
+			PolicyAssignmentName string   `json:"policy_assignment_name" yaml:"policy_assignment_name"`
+			ParameterNames       []string `json:"parameter_names" yaml:"parameter_names"`
+		} `json:"policy_assignments" yaml:"policy_assignments"`
+	} `json:"defaults" yaml:"defaults"`
 }
