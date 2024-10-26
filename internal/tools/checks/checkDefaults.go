f9a5fa8c8dab9bff10020fd3dc3fade48421df28
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-10-26 11:01:48 +0100

    feat: refactor check defaults to reference the definition rather then the assignment

--- internal/tools/checks/checkDefaults.go
+++ internal/tools/checks/checkDefaults.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/Azure/alzlib"
 	"github.com/Azure/alzlib/internal/tools/checker"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 )
 
 var CheckDefaults = checker.NewValidatorCheck("All defaults are valid", checkDefaults)
@@ -25,10 +26,21 @@ func checkDefaults(azany any) error {
 			if a == nil {
 				return fmt.Errorf("checkDefaults: policy assignment `%s`, referenced by default `%s` is not found in the library", assignment, def)
 			}
-			paramNames := pdv.AssignmentParameters(assignment)
-			for _, paramName := range paramNames {
-				if _, ok := a.Properties.Parameters[paramName]; !ok {
-					return fmt.Errorf("checkDefaults: parameter `%s` in assignment `%s`, referenced by default `%s` is not found in the library", paramName, assignment, def)
+			// We need to check that the referenced definition has the parameter as it may not be present in the assignment (e.g. if it has a default value)
+			// First let's get the referenced policy definition id and parse it into a resource id type.
+			var pdIdStr string
+			if a.Properties.PolicyDefinitionID == nil {
+				return fmt.Errorf("checkDefaults: policy assignment `%s`, referenced by default `%s` does not have a policy definition ID", assignment, def)
+			}
+			pdIdStr = *a.Properties.PolicyDefinitionID
+			pdResId, err := arm.ParseResourceID(pdIdStr)
+			if err != nil {
+				return fmt.Errorf("checkDefaults: policy assignment `%s`, referenced by default `%s` has an invalid policy definition ID", assignment, def)
+			}
+			// Now we can check that the parameters are present in the referenced definition
+			for _, param := range pdv.AssignmentParameters(assignment) {
+				if !az.AssignmentReferencedDefinitionHasParameter(pdResId, param) {
+					return fmt.Errorf("checkDefaults: policy assignment `%s`, referenced by default `%s` has a parameter `%s` that is not present in the referenced definition", assignment, def, param)
 				}
 			}
 		}
---
+++ internal/tools/checks/checkDefaults_test.go
@@ -0,0 +1,40 @@
+package checks
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Azure/alzlib"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCheckDefaultsGood(t *testing.T) {
+	az := alzlib.NewAlzLib(nil)
+	ctx := context.Background()
+	lib := alzlib.NewCustomLibraryReference("testdata/defaultsgood")
+	_, err := lib.Fetch(ctx, "0")
+	require.NoError(t, err)
+	require.NoError(t, az.Init(ctx, lib))
+	assert.NoError(t, checkDefaults(az))
+}
+
+func TestCheckDefaultsAssignmentNotPresent(t *testing.T) {
+	az := alzlib.NewAlzLib(nil)
+	ctx := context.Background()
+	lib := alzlib.NewCustomLibraryReference("testdata/defaultsassignmentnotpresent")
+	_, err := lib.Fetch(ctx, "0")
+	require.NoError(t, err)
+	require.NoError(t, az.Init(ctx, lib))
+	assert.ErrorContains(t, checkDefaults(az), "policy assignment `not_present`, referenced by default `test` is not found in the library")
+}
+
+func TestCheckDefaultsParameterNotPresent(t *testing.T) {
+	az := alzlib.NewAlzLib(nil)
+	ctx := context.Background()
+	lib := alzlib.NewCustomLibraryReference("testdata/defaultsparameternotpresent")
+	_, err := lib.Fetch(ctx, "0")
+	require.NoError(t, err)
+	require.NoError(t, az.Init(ctx, lib))
+	assert.ErrorContains(t, checkDefaults(az), "policy assignment `test-policy-assignment`, referenced by default `test` has a parameter `not_present` that is not present in the referenced definition")
+}
