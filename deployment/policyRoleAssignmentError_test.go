15383241bf6fb8129d6d699995a640cfba44d252
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-11-08 13:30:06 +0000

    test: fix test expected output

--- deployment/policyRoleAssignmentError_test.go
+++ deployment/policyRoleAssignmentError_test.go
@@ -15,7 +15,7 @@ func TestPolicyRoleAssignmentErrors_Error(t *testing.T) {
 	err2 := NewPolicyRoleAssignmentError("testAssignment2", "testScope2", "testParameter2", "pdRef2", []string{"role3", "role4"})
 	errors := PolicyRoleAssignmentErrors{
 		errors: []*PolicyRoleAssignmentError{err1, err2}}
-	expected := "PolicyRoleAssignmentError: could not generate role assignment for assignment `testAssignment1` assigned at scope `testScope1`. A new role assignment should be created at scope of the definition referenced by `pdRef1`, using parameter name `testParameter1`, for the following role definition ids: `role1, role2`\nPolicyRoleAssignmentError: could not generate role assignment for assignment `testAssignment2` assigned at scope `testScope2`. A new role assignment should be created at scope of the definition referenced by `pdRef2`, using parameter name `testParameter2`, for the following role definition ids: `role3, role4`"
+	expected := "PolicyRoleAssignmentError: could not generate role assignment for assignment `testAssignment1` assigned at scope `testScope1`. A new role assignment should be created at scope of the definition referenced by `pdRef1`, using parameter name `testParameter1`, for the following role definition ids: `role1, role2`\n---\nPolicyRoleAssignmentError: could not generate role assignment for assignment `testAssignment2` assigned at scope `testScope2`. A new role assignment should be created at scope of the definition referenced by `pdRef2`, using parameter name `testParameter2`, for the following role definition ids: `role3, role4`"
 	if errors.Error() != expected {
 		t.Errorf("expected %s, got %s", expected, errors.Error())
 	}
