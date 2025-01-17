b3d041c656cdc8a6bcb5f077b8d324d4c6afb738
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-17 11:59:15 +0000

    chore: spelling

--- cmd/alzlibtool/command/document/document.go
+++ cmd/alzlibtool/command/document/document.go
@@ -9,7 +9,7 @@ import (
 var DocumentBaseCmd = cobra.Command{
 	Use:   "document",
 	Short: "Generates documentation for alzlib resources.",
-	Long:  `Produces documentation for alzlib resourcesm, curreltly only library members supported.`,
+	Long:  `Produces documentation for alzlib resources, currently only library members supported.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.PrintErrf("%s document command: missing required child command\n", cmd.ErrPrefix())
 		cmd.Usage() // nolint: errcheck
